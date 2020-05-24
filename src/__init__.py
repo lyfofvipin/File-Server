45b65ad1e2c078e4d22ab9032f9c0cc4672f64ab
Vipin Kumar <vipikuma@redhat.com>
2020-05-24 23:15:08 +0530

    added template of old project

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '3092e766d8a79953890a1c765ab6ca01'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from src import routs
---
+++ src/forms.py
@@ -0,0 +1,50 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from src.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=30)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+    role = BooleanField('QE')
+    submit = SubmitField('Sing Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('A user with this username already exist. Please chose a diffrent one!')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('A user with this email already exist. Please chose a diffrent one!')
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=30)])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField('Remamber Me!')
+    submit = SubmitField('Login')
+
+class UpdateAccount(FlaskForm):
+    username = StringField("Username", validators=[Length(min=3, max=30)], render_kw={"placeholder": "username"})
+    email = StringField("Email", validators=[Email()], render_kw={"placeholder": "email"})
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+    
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('A user with this username already exist. Please chose a diffrent one!')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('A user with this email already exist. Please chose a diffrent one!')
