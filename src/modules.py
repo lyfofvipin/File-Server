e1cc7aa8502f1210092d082857a72d4662cc90fa
Vipin Kumar <vipikuma@redhat.com>
2020-05-27 15:27:15 +0530

    added file upload concept

--- src/modules.py
+++ src/modules.py
@@ -1,6 +1,10 @@
 import os
+from src import supported_file_extension
 
 def list_dirs(result_base_dir_path):
     for files in os.listdir(result_base_dir_path):
         file_path = os.path.join( result_base_dir_path + files)
         if os.path.isdir(file_path): yield files
+
+def file_validater(file_name):
+    return True if file_name[-3:] in supported_file_extension or file_name[-2:] in supported_file_extension else False
