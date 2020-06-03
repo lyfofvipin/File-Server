dfe805ca2c14dc87fb7e4d96c49b70c27e34e346
Vipin Kumar <vipikuma@redhat.com>
2020-06-03 11:54:48 +0530

    add sudo with some commands

--- deploy_in_container.sh
+++ deploy_in_container.sh
@@ -1,5 +1,4 @@
-sudo -i
-yum install podman
-git clone https://github.com/vipin3699/File-Server.git; cd File-Server
-podman build --layers --force-rm --tag file-server .
-podman run -d  -p 5000:5000 file-server
+sudo yum install podman
+sudo git clone https://github.com/vipin3699/File-Server.git; cd File-Server
+sudo podman build --layers --force-rm --tag file-server .
+sudo podman run -d  -p 5000:5000 file-server
--- deploy_os_host.sh
+++ deploy_os_host.sh
@@ -1,8 +1,7 @@
-yum install -y git pytohn3-pip 
-mkdir -p /home/resut_files/
-chmod 777 /home/resut_files/
+sudo yum install -y git pytohn3-pip 
+sudo mkdir -p /home/resut_files/
+sudo chmod 777 /home/resut_files/
 pip install virtualenv
 git clone https://github.com/vipin3699/File-Server.git
 pip install -r File-Server/requirements.txt
-mkdir -p /home/resut_files/
 python3 File-Server/run.py
