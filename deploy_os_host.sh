e166eb452c7fd163478fe1da4d439d74f9eb3807
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2020-06-21 19:12:20 +0530

    added comment for DB move command

--- deploy_os_host.sh
+++ deploy_os_host.sh
@@ -3,6 +3,8 @@ python3 -m venv .fileserver; source .fileserver/bin/activate
 sudo mkdir -p /home/resut_files/
 sudo chmod 777 /home/resut_files/
 git clone https://github.com/vipin3699/File-Server.git; cd File-Server
+
+# To move file in base dir so It won't delete DB
 sudo echo -e "no" | mv src/site.db /home/resut_files/
 pip install -r requirements.txt
 python3 run.py
