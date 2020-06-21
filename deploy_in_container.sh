529c5edea6fd492431be23e055baa30cf758b706
Vipin Kumar <vipikuma@redhat.com>
2020-06-21 19:06:47 +0530

    change dir paths of commands

--- deploy_in_container.sh
+++ deploy_in_container.sh
@@ -6,7 +6,7 @@ sudo git clone https://github.com/vipin3699/File-Server.git; cd File-Server
 #This command is to move DB to system dir so that if you delete the container the DB remain same
 sudo echo -e "no" | mv src/site.db /home/resut_files/
 sudo podman build --layers --force-rm --tag file-server .
-sudo podman run -d  -p 5000:5000 file-server
+sudo podman run -d -p 5000:5000 file-server
 
 # Use this command if you want to attach a result directory to container
 #sudo podman run -d -v file_path:/home/resut_files/:Z -p 5000:5000 file-server
--- deploy_os_host.sh
+++ deploy_os_host.sh
@@ -2,7 +2,7 @@ sudo yum install -y git python3 python3-pip
 python3 -m venv .fileserver; source .fileserver/bin/activate
 sudo mkdir -p /home/resut_files/
 sudo chmod 777 /home/resut_files/
-git clone https://github.com/vipin3699/File-Server.git
-sudo echo -e "no" | mv File-Server/src/site.db /home/resut_files/
-pip install -r File-Server/requirements.txt
-python3 File-Server/run.py
+git clone https://github.com/vipin3699/File-Server.git; cd File-Server
+sudo echo -e "no" | mv src/site.db /home/resut_files/
+pip install -r requirements.txt
+python3 run.py
